e04c5ce3ac784e1e5247fff612dc9115a3833eb3
Lawood <erwann.sale@laposte.net>
2018-04-24 16:05:28 +0200

    add class ffdec

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { spawn } from 'child_process';
 
-const cmd = spawn('java', '-jar ffdec.jar -export script ./output/ DofusInvoker.swf'.split(' '));
+const cmd = spawn('java', '-jar ffdec.jar -selectclass com.ankamagames.dofus.BuildInfos,com.ankamagames.dofus.network.++,com.ankamagames.jerakine.network.++  -export script ./output/ DofusInvoker.swf'.split(' '));
 
 // const cmd = spawn('ls', ['-lh', '/usr']);
 
