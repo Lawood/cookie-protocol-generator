383943b39ecac4ba5b1cd3279ce9350ab4dc8afc
Christopher YOVANOVITCH <yovano_c@outlook.com>
2018-04-24 16:14:14 +0200

    cleaning

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 import { spawn } from 'child_process';
 
-const cmd = spawn('java', '-jar ffdec.jar -selectclass com.ankamagames.dofus.BuildInfos,com.ankamagames.dofus.network.++,com.ankamagames.jerakine.network.++  -export script ./output/ DofusInvoker.swf'.split(' '));
+const selected = [
+  'com.ankamagames.dofus.BuildInfos',
+  'com.ankamagames.dofus.network.++',
+  'com.ankamagames.jerakine.network.++',
+].join(',');
+
+const cmd = spawn('java', `-jar ffdec.jar -selectclass ${selected} -export script ./output/ DofusInvoker.swf`.split(' '));
 
 // const cmd = spawn('ls', ['-lh', '/usr']);
 
